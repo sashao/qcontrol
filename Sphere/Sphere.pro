dc58fcf1b9a832ebdf9a5fd36a41915441c8f90f
Oleksandr <sashao@e-mail.ua>
2009-03-11 14:04:47 +0200

    Moved from easytrans

---
+++ Sphere/Sphere.pro
@@ -0,0 +1,16 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2009-03-05T22:25:54
+#
+#-------------------------------------------------
+
+QT       += script svg xml
+
+TARGET = Sphere
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
---
+++ Sphere/main.cpp
@@ -0,0 +1,14 @@
+#include <QtGui/QApplication>
+#include <QDebug>
+#include "widget.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    
+    qDebug()<<QCoreApplication::instance();
+    qDebug("sph === %d",QCoreApplication::instance());
+    return a.exec();
+}
